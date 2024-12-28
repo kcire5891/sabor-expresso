4e7571545bca0d6bc01939dec386a4fdb62e0a7a
Erick <erickbarretodasilva@gmail.com>
2024-12-28 13:37:04 -0300

    básico

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-# print('Sabor Express\n')
+import os
 
-print("""
+def exibir_nome_do_programa():
+    print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -9,10 +10,64 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
 """)
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print('Você escolheu a opção', opcao_escolhida)
+def opcao_invalida():
+    print('Opção Inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
+
+def finalizar_app():
+    os.system('clear')
+    # s.system('clear') no mac
+    print('Finalizando o app')
+
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # print('Você escolheu a opção', opcao_escolhida)
+        # print(f'Você escolheu a opção {opcao_escolhida}')
+        # print(type(opcao_escolhida))
+        # print(type(1))
+
+        if opcao_escolhida == 1:
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurantes')
+        elif opcao_escolhida == 4: 
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except: 
+        opcao_invalida()
+
+# def escolher_opcao_match():
+#     opcao_escolhida = int(input('Escolha uma opção: '))
+#     match opcao_escolhida:
+#         case 1:
+#             print('Adicionar restaurante')
+#         case 2:
+#             print('Listar restaurantes')
+#         case 3:
+#             print('Ativar restaurante')
+#         case 4:
+#             print('Finalizar app')
+#         case _:
+#             print('Opção inválida!')
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+    # escolher_opcao_match()
+
+    
+if __name__ == "__main__":
+    main()
